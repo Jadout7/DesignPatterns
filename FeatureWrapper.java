f3e966ded2b4cc82388aa626a8a09fefb224efcd
JadynJacques <91316558+Jadout7@users.noreply.github.com>
2024-03-19 12:34:39 +0100

    Updated to Asta class diagram

--- FeatureWrapper.java
+++ FeatureWrapper.java
@@ -1,4 +1,4 @@
-public abstract class FeatureWrapper {
+public abstract class FeatureWrapper { // Abstract? Interface?
     private Car car;
     private double price;
     private double taxAmount;
