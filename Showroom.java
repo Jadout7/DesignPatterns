750d539d79966e5569a0b26de1f8e8b29ad00349
Andreikova <112638712+Andreikova@users.noreply.github.com>
2024-03-20 16:25:19 +0100

    Create Showroom.java

---
+++ Showroom.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Showroom {
+    private ArrayList<Car> cars;
+    private ArrayList<Car> soldCars;
+
+    public Showroom(ArrayList<Car> cars, ArrayList<Car> soldCars) {
+        this.cars = cars;
+        this.soldCars = soldCars;
+    }
+
+    //TODO: This method to place order
+
+
+    // public void placeOrder(Order order)
+
+    //TODO: purchasing multiple cars method
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void addCar(Car car){
+        this.cars.add(car);
+    }
+
+    public void removeCar(Car car){
+        cars.remove(car);
+    }
+
+    public ArrayList<Car> getSoldCars() {
+        return soldCars;
+    }
+}
