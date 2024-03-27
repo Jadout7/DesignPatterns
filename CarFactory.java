5d3abca82e4623c0ebdeb7faabbe48855fbb668e
Andreikova <112638712+Andreikova@users.noreply.github.com>
2024-03-27 23:41:35 +0100

    Added the (simple) factory class
    *It uses type cast once because i dont know a work around atm*
    
    -Checks the object passed and makes the car

--- CarFactory.java
+++ CarFactory.java
@@ -1,2 +1,19 @@
 public class CarFactory {
+
+    public Car createCar(Car carType){
+        if (carType==null){
+            return null;
+        }
+        //TODO: Ask about should a simple factory be used or factory method or Abstract factory
+
+        if(carType instanceof ElectricCar){
+            return new ElectricCar(carType.getVin(),carType.getBrand(),carType.getModel(), carType.getYearMade(), carType.getPrice(), 100);
+        } else if (carType instanceof PetrolCar) {
+            //I apologise for the typecast but it *should work*
+            return new PetrolCar(carType.getVin(),carType.getBrand(),carType.getModel(), carType.getYearMade(), carType.getPrice(), ((PetrolCar) carType).getFuelType());
+        } else if (carType instanceof HydrogenCar){
+            return new HydrogenCar(carType.getVin(),carType.getBrand(),carType.getModel(), carType.getYearMade(), carType.getPrice(), 100);
+        } else return null;
+
+    }
 }
--- Hydrogen.java
+++
@@ -1,8 +0,0 @@
-public class Hydrogen extends Car {
-
-    public Hydrogen(long vin, String brand, String model, int yearMade, float price) {
-        super(vin, brand, model, yearMade, price);
-    }
-
-
-}
