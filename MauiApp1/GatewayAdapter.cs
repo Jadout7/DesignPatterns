5979e34dac578fdc821a08f3b483714c634cd08e
Nathan Mills <91469944+pepehonourable@users.noreply.github.com>
2024-03-30 14:38:56 +0100

    Made maui project. Error fixing
    Fixed errors related to Adapter

---
+++ MauiApp1/GatewayAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class GatewayAdapter : IPayable
+{
+    private IDeal IdealAdapter;
+
+    public GatewayAdapter(IDeal idealAdapter)
+    {
+        IdealAdapter = idealAdapter;
+    }
+
+    public bool ProcessPayment()
+    {
+        return IdealAdapter.IsValidIban() && IdealAdapter.IsValidBank() && IdealAdapter.IsValidAccountName();
+    }
+}
