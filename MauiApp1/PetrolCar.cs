7e3843a75dbcf3c58182d38ddc928a74844532a1
JadynJacques <91316558+Jadout7@users.noreply.github.com>
2024-04-06 13:02:01 +0200

    Attempt at Nathan's binding issue

--- MauiApp1/PetrolCar.cs
+++ MauiApp1/PetrolCar.cs
@@ -2,6 +2,13 @@
 
 public class PetrolCar : Car
 {
+    private long Vin { get; set; }
+    private string Brand { get; set; }
+    private string Model { get; set; }
+    private int YearMade { get; set; }
+    private float Price { get; set; }
+    private string FuelType { get; set; }
+
     public PetrolCar(long vIN, string brand, string model, int yearMade, float price, string fuelType) : base(vIN, brand, model, yearMade, price)
     {
     }
--- MauiApp1/ShoppingCart.xaml.cs
+++ MauiApp1/ShoppingCart.xaml.cs
@@ -6,20 +6,8 @@ namespace MauiApp1;
 public partial class ShoppingCart : ContentPage
 {
     public ShoppingCart()
-	{
+    {
         InitializeComponent();
         BindingContext = new ShoppingCartViewModel(); // Assuming you have a view model named MainPageViewModel
     }
-
-    // Method to get the collection of PetrolCars
-/*    private List<PetrolCar> GetPetrolCars()
-    {
-        // Implement logic to fetch PetrolCar instances, for example from a database
-        // This is a dummy implementation
-        return new List<PetrolCar>
-        {
-            new PetrolCar(123456, "Brand1", "Model1", 2022, 20000f, "Petrol"),
-            new PetrolCar(789012, "Brand2", "Model2", 2023, 25000f, "Petrol")
-        };
-    }*/
 }
