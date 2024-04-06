f8d9eda6ddf3dc6576f4d81e1c15bbe8a4073072
Nathan Mills <91469944+pepehonourable@users.noreply.github.com>
2024-04-07 00:10:00 +0200

    Revert "Actual GitIgnore"
    This reverts commit 33ea1a9da63f2729d0130df6a04e123605779659.

--- MauiApp1/obj/Debug/net8.0-android/CarShowroom.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-android/CarShowroom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarShowroom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+588081704048097a822802d06e9dc04185db2628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+51d4b1427bd751bff22a103f5eefe6deb2f786a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarShowroom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarShowroom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
