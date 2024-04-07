20db0068c235ba1cc6d59f131e47261484e5f11f
JadynJacques <91316558+Jadout7@users.noreply.github.com>
2024-04-07 20:52:32 +0200

    Static Feature implementation

---
+++ MauiApp1/Exhaust.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Exhaust : CarDecorator
+{
+    public Exhaust(Car car) : base(car)
+    {
+        Price = 500;
+    }
+}
+
--- MauiApp1/Features.xaml.cs
+++ MauiApp1/Features.xaml.cs
@@ -1,21 +1,42 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace MauiApp1;
 
 public partial class Features : ContentPage
 {
-	public Features()
+
+    public Features()
 	{
 		InitializeComponent();
-	}
-
-    private void OnViewClicked(object sender, EventArgs e)
-    {
-
     }
 
-    void OnAddClicked(object sender, EventArgs e)
+    void OnAddExhaustClicked(object sender, EventArgs e)
     {
         // Add the feature to the cart
         // This will depend on your implementation
+        DisplayAlert("Feature Adding", "Exhaust Added", "OK");
+    }
+
+    void OnAddSportTiresClicked(object sender, EventArgs e)
+    {
+        // Add the feature to the cart
+        // This will depend on your implementation
+        DisplayAlert("Feature Adding", "Sport Tires Added", "OK");
+    }
+
+    void OnAddSpoilerClicked(object sender, EventArgs e)
+    {
+        // Add the feature to the cart
+        // This will depend on your implementation
+        DisplayAlert("Feature Adding", "Spoiler Added", "OK");
+    }
+
+    void OnAddUnderglowClicked(object sender, EventArgs e)
+    {
+        // Add the feature to the cart
+        // This will depend on your implementation
+        DisplayAlert("Feature Adding", "Underglow Added", "OK");
     }
 
     void OnContinueClicked(object sender, EventArgs e)
